3293207cb0145a0d2bb105c3007c9a864401ce62
tsu <tsu@yaroot.net>
2018-01-28 15:58:38 +0800

    fix resource cleanup

--- src/main/scala/gitbucket/plugin/metrics/package.scala
+++ src/main/scala/gitbucket/plugin/metrics/package.scala
@@ -11,16 +11,22 @@ import org.apache.commons.io.FileUtils
 
 import scala.concurrent.duration._
 
-// the lifecycle is rather simple:
-//   - upon start, we create a `ScheduledThreadPoolExecutor` for running periodic/io-related tasks
-//   - during shutdown, we close the executor
+/*
+ the lifecycle is rather simple:
+   - upon start, we create a `ScheduledThreadPoolExecutor` for running periodic/io-related tasks
+   - during shutdown, we close the executor
+
+ TODO:
+ - list all repos & related resources (LFS/uploads)
+*/
 class RepositoryMetrics {
   import RepositoryMetrics._
 
   // we run any IO related task on this executor
   val scheduledExecutor = new ScheduledThreadPoolExecutor(1)
 
-  @volatile var mbeans = Map.empty[ObjectName, () => Unit]
+  val mbeanServer = defaultMBeanServer()
+  @volatile var mbeans = Map.empty[ObjectName, CleanUp]
 
   // we execute everything on the scheduler, so it's fine that `f` throws exception
   def schedule(f: () => Unit): Scheduled = {
@@ -34,23 +40,21 @@ class RepositoryMetrics {
     () => delay.cancel(false)
   }
 
-  // TODO support cleanup
-  def registerMBean(obj: Object, name: ObjectName): Unit = {
-    // gitbucket may reload plugins, and it seems the shutdown will not be called before reload
-    // so we have to check here, moreover there will be resource leaks
-    if (mbeanServer().isRegistered(name)) {
-      mbeanServer().unregisterMBean(name)
+  def registerMBean(obj: Object, name: ObjectName, cleanup: CleanUp): Unit = {
+    if (mbeanServer.isRegistered(name)) {
+      mbeanServer.unregisterMBean(name)
     }
-    val instance = mbeanServer().registerMBean(obj, name)
+    val instance = mbeanServer.registerMBean(obj, name)
     val name0 = Option(instance).map(_.getObjectName).getOrElse(name)
-    mbeans += (name0, () => Unit)
+    mbeans += (name0 -> cleanup)
   }
 
   def unregisterMBean(name: ObjectName): Unit = {
     try {
-      mbeans.get(name).foreach(f => f())
+      mbeans.get(name).foreach(_.cleanup())
     } finally {
       mbeans -= name
+      mbeanServer.unregisterMBean(name)
     }
   }
 
@@ -63,9 +67,9 @@ class RepositoryMetrics {
     val refresh = () => atomicLong.set(directorySize(baseDir))
     val mbean = new RepoSize(atomicLong.get(), refresh)
 
-    schedule(refresh)
+    val scheduled = schedule(refresh)
 
-    registerMBean(mbean, objectName(SIZE_DOMAIN, "total"))
+    registerMBean(mbean, objectName(SIZE_DOMAIN, "total"), () => scheduled.cancel())
   }
 
   def shutdown(): Unit = {
@@ -78,13 +82,13 @@ class RepositoryMetrics {
 }
 
 object RepositoryMetrics {
-  val DOMAIN = "gitbucket"
-  val SIZE_DOMAIN = s"$DOMAIN.directory.repository"
+  val DOMAIN = "io.github.gitbucket"
+  val SIZE_DOMAIN = s"$DOMAIN.repository.size"
   val FIXED_DELAY: FiniteDuration = 1.hour
 
   def baseDir: Path = Paths.get(Directory.RepositoryHome).toAbsolutePath
 
-  def mbeanServer(): MBeanServer = ManagementFactory.getPlatformMBeanServer
+  def defaultMBeanServer(): MBeanServer = ManagementFactory.getPlatformMBeanServer
 
   def directorySize(path: Path): Long = FileUtils.sizeOfDirectory(path.toFile)
 
@@ -93,6 +97,10 @@ object RepositoryMetrics {
     if (obj.isPattern) new ObjectName(domain, "name", ObjectName.quote(name))
     else obj
   }
+}
+
+trait CleanUp {
+  def cleanup(): Unit
 }
 
 trait Scheduled {
